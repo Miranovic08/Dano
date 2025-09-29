91e26701d3b98fddd59cd5b2a671db05edebcfcc
Miranovic08 <miranovic2802@gmail.com>
2025-09-29 23:22:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    const duration = 3 * 450;
+    const end = Date.now() + duration;
+
+    (function frame() {
+      confetti({
+        particleCount: 3,
+        angle: 315,
+        spread: 85,
+        origin: { y: .0,x:0 },
+      });
+      confetti({
+        particleCount: 3,
+        angle: 225,
+        spread: 85,
+        origin: { y: .0, x: 1 },
+      });
+
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    })();
+  };
